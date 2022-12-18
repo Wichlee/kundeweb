327a0281e921902a40bac4b670ea4a4da1a075bf
2bagg <gutu1011@h-ka.de>
2022-12-18 15:33:09 +0100

    add suchergebnis.component

---
+++ src/app/kunde/suche-kunden/suchergebnis/suchergebnis.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { type Kunde } from '../../shared/kunde';
+import { ErrorMessageComponent } from '../../../shared/error-message.component';
+import { GefundeneKundenComponent } from './gefundene-kunden.component';
+import { NgIf } from '@angular/common';
+import log from 'loglevel';
+
+/**
+ * Komponente f&uuml;r den CSS-Selektor <code>hs-suchergebnis</code>, um das Ergebnis der
+ * Suche anzuzeigen, d.h. die gefundenen Kunden oder eine Fehlermeldung.
+ */
+@Component({
+    selector: 'hs-suchergebnis',
+    templateUrl: './suchergebnis.component.html',
+    imports: [ErrorMessageComponent, NgIf, GefundeneKundenComponent],
+    standalone: true,
+})
+export class SuchergebnisComponent {
+    @Input()
+    buecher: Kunde[] = [];
+
+    @Input()
+    errorMsg: string | undefined;
+
+    constructor() {
+        log.debug('SuchergebnisComponent.constructor()');
+    }
+}
