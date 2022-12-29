28df54cea86b77144dfad48cf7ebe8a981b4639a
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2022-12-29 18:24:10 +0100

    add: constructor

--- src/app/kunde/create-kunde/kundeForm.ts
+++ src/app/kunde/create-kunde/kundeForm.ts
@@ -1,5 +1,28 @@
-import { type Kunde, type KundeShared } from '../shared/kunde';
+import { type Kunde type KundeShared } from '../shared/kunde';
 
 export interface KundeForm extends KundeShared {
+
+}
+
+export const toKunde = (kundeForm: KundeForm): Kunde => {
+  log.debug('toKunde: kundeForm=', kundeForm);
+
+  const {
+    nachname,
+    email,
+    kategorie,
+    hasNewsletter,
+    geburtsdatum,
+    homepage,
+    rabatt,
+    geschlecht,
+    familienstand,
+    sport, // Interessen als bools, da diese eine Liste sind -> siehe kunde.ts
+    lesen,
+    reisen,
+    umsatz,
+    adresse,
+    username,
+  } = kundeForm;
 
 }
