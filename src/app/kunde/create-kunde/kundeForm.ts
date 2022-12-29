39b4c3a4119c5e8b7bb107f2789878da454b9eb0
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2022-12-29 19:25:59 +0100

    add: imports and description

--- src/app/kunde/create-kunde/kundeForm.ts
+++ src/app/kunde/create-kunde/kundeForm.ts
@@ -1,5 +1,14 @@
-import { type Kunde type KundeShared } from '../shared/kunde';
+import { type Kunde, type KundeShared } from '../shared/kunde';
+import { Temporal } from '@js-temporal/polyfill';
+import log from 'loglevel';
 
+/**
+ * Daten aus einem Formular:
+ * <ul>
+ *  <li> je 1 Control fuer jede Checkbox und
+ *  <li> außerdem Strings für Eingabefelder für Zahlen.
+ * </ul>
+ */
 export interface KundeForm extends KundeShared {
   geburtsdatum: Date;
   sport: boolean;
@@ -7,6 +16,10 @@ export interface KundeForm extends KundeShared {
   reisen: boolean;
 }
 
+/**
+ * Ein Kunde-Objekt mit JSON-Objekt aus einem Formular erzeugen
+ * @param kunde JSON-Objekt mit Daten aus dem Formular
+ */
 export const toKunde = (kundeForm: KundeForm): Kunde => {
   log.debug('toKunde: kundeForm=', kundeForm);
 
