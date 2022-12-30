46dc570c37e137f734d079c044974c116b788aac
Erik Dwornik <dwer1011@h-ka.de>
2022-12-30 13:51:07 +0100

    added routers

---
+++ src/app/routes.ts
@@ -0,0 +1,23 @@
+import { HomeComponent } from './home/home.component';
+import { type Routes } from '@angular/router';
+
+// Route-Definitionen fuer den Root-Router
+// Eine Route leitet zu einer neuen Ansicht ("View") in der SPA, wobei die
+// vorherige Ansicht ueberdeckt bzw. ausgeblendet wird.
+export const ROUTES: Routes = [
+    {
+        path: '',
+        redirectTo: '',
+        // redirect erfordert pathMatch full
+        pathMatch: 'full',
+        component: HomeComponent,
+        title: 'Beispiel Gruppe2',
+    },
+    {
+        path: 'kunden',
+        // Lazy Loading (statt Eager Loading) durch dynamische Imports (seit ES 2020)
+        // loadChildren statt component wie bei '/'
+        // https://angular.io/guide/lazy-loading-ngmodules
+        loadChildren: () => import('./kunde/routes').then(mod => mod.ROUTES),
+    },
+];
