d62d26df93ea719e4d7eaf8fe413c113e6aa01af
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2023-01-03 12:42:47 +0100

    add: import directives

---
+++ src/app/kunde/create-kunde/create-email.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, type OnInit } from '@angular/core';
+import {
+    FormControl,
+    type FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
+import log from 'loglevel';
+
