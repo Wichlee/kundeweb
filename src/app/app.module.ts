1aa1494695884684f59a694b707814debb3746d0
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2023-01-03 12:08:02 +0100

    add: angular-material as app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,8 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+
+@NgModule({ imports: [MatButtonModule, MatIconModule] })
+export class AppModule {}
+
+// todo: Anuglar Material zum laufen bringen
