50b75da7eb6206461168b23134462c37ad13fac9
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2023-01-04 14:35:43 +0100

    add: component hasNewsletter

---
+++ src/app/kunde/create-kunde/create-hasNewsletter.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, type OnInit } from '@angular/core';
+import {
+    FormControl,
+    type FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import log from 'loglevel';
+
+@Component({
+    selector: 'hs-create-has-newsletter',
+    templateUrl: './create-hasNewsletter.component.html',
+    imports: [FormsModule, ReactiveFormsModule],
+    standalone: true,
+})
+export class CreateHasNewsletterComponent implements OnInit {
+    @Input()
+    form!: FormGroup;
+
+    hasNewsletter = new FormControl(false);
+
+    ngOnInit() {
+        log.debug('CreateHasNewsletterComponent.ngOnInit');
+        this.form.addControl('hasNewsletter', this.hasNewsletter);
+    }
+}
