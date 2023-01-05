800ffa35421b787fdf17d2762ba68a171d616d6f
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2023-01-05 15:46:27 +0100

    add: component familienstand

---
+++ src/app/kunde/create-kunde/create-familienstand.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, type OnInit } from '@angular/core';
+import {
+    FormControl,
+    type FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
+import log from 'loglevel';
+
+/**
+ * Komponente f&uuml;r den CSS-Selektor &lt;hs-create-familienstand&gt;, um Erfassungsformular f&uuml;r einen neun Kunden zu realisieren.
+ */
+@Component({
+    selector: 'hs-create-familienstand',
+    templateUrl: './create-familienstand.component.html',
+    imports: [FormsModule, NgIf, ReactiveFormsModule],
+    standalone: true,
+})
+export class CreateFamilienstandComponent implements OnInit {
+    @Input()
+    form!: FormGroup;
+
+    familienstand = new FormControl(undefined, Validators.required);
+
+    ngOnInit() {
+        log.debug('CreateFamilienstandComponent.ngOnInit');
+        this.form.addControl('familienstand', this.familienstand);
+    }
+}
