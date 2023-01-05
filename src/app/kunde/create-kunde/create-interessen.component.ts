91e5b9b2fc35da212206796db73a202bf9d0633a
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2023-01-05 16:05:10 +0100

    add: component interessen

---
+++ src/app/kunde/create-kunde/create-interessen.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, type OnInit } from '@angular/core';
+import {
+    FormControl,
+    type FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+} from '@angular/forms';
+import log from 'loglevel';
+
+@Component({
+    selector: 'hs-create-interessen',
+    templateUrl: './create-interessen.component.html',
+    imports: [FormsModule, ReactiveFormsModule],
+    standalone: true,
+})
+export class CreateInteressenComponent implements OnInit {
+    @Input()
+    form!: FormGroup;
+
+    protected readonly sport = new FormControl(false);
+
+    protected readonly lesen = new FormControl(false);
+
+    protected readonly reisen = new FormControl(false);
+
+    ngOnInit() {
+        log.debug('CreateInteressenComponent.ngOnInit');
+        this.form.addControl('sport', new FormControl(false));
+        this.form.addControl('lesen', new FormControl(false));
+        this.form.addControl('reisen', new FormControl(false));
+    }
+}
