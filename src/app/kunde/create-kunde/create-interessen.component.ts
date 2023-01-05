b011925623f39eafa3adfb727b202d9954f54631
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2023-01-05 16:11:48 +0100

    add: comment

--- src/app/kunde/create-kunde/create-interessen.component.ts
+++ src/app/kunde/create-kunde/create-interessen.component.ts
@@ -7,6 +7,9 @@ import {
 } from '@angular/forms';
 import log from 'loglevel';
 
+/**
+ * Komponente f&uuml;r den CSS-Selektor &lt;hs-create-interessen&gt;, um Erfassungsformular f&uuml;r einen neun Kunden zu realisieren.
+ */
 @Component({
     selector: 'hs-create-interessen',
     templateUrl: './create-interessen.component.html',
