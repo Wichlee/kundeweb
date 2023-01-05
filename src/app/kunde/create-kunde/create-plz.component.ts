cb95adc00dc47806578b68f00c97730382444ffe
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2023-01-05 19:15:04 +0100

    add: component plz

---
+++ src/app/kunde/create-kunde/create-plz.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, type OnInit } from '@angular/core';
+import {
+    FormControl,
+    type FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { PLZ_REGEX } from '../shared/kunde';
+import log from 'loglevel';
+
+/**
+ * Komponente f&uuml;r den CSS-Selektor &lt;hs-create-plz&gt;, um Erfassungsformular f&uuml;r einen neuen Kunden zu realisieren.
+ */
+@Component({
+    selector: 'hs-create-plz',
+    templateUrl: './create-plz.component.html',
+    imports: [FormsModule, NgIf, ReactiveFormsModule],
+    standalone: true,
+})
+export class CreatePlzComponent implements OnInit {
+    @Input()
+    form!: FormGroup;
+
+    protected readonly plz = new FormControl(undefined, [
+        Validators.required,
+        Validators.pattern(PLZ_REGEX),
+    ]);
+
+    ngOnInit() {
+        log.debug('CreatePlzComponent.ngOnInit');
+        this.form.addControl('plz', this.plz);
+    }
+}
