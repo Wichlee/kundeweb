fc22639edcb7dc56abddc851fd5501d3aa520037
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2023-01-05 16:15:39 +0100

    add: component umsatz

---
+++ src/app/kunde/create-kunde/create-umsatz.component.ts
@@ -0,0 +1,31 @@
+import { Component, Component, Input, type OnInit } from '@angular/core';
+import {
+    FormControl,
+    type FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
+import log from 'loglevel';
+
+/**
+ * Komponente f&uuml;r den CSS-Selektor &lt;hs-create-umsatz&gt;, um Erfassungsformular f&uuml;r einen neuen Kunden zu realisieren.
+ */
+@Component({
+    selector: 'hs-create-umsatz',
+    templateUrl: './create-umsatz.component.html',
+    imports: [FormsModule, NgIf, ReactiveFormsModule],
+    standalone: true,
+})
+export class CreateUmsatzComponent implements OnInit {
+    @Input()
+    form!: FormGroup;
+
+    protected readonly umsatz = new FormControl(undefined, Validators.required);
+
+    ngOnInit() {
+        log.debug('CreateUmsatzComponent.ngOnInit');
+        this.form.addControl('umsatz', this.umsatz);
+    }
+}
