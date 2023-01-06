3f240cc5de35ff60e75c6ce6294aa4a77d0cf8ca
2bagg <gutu1011@h-ka.de>
2023-01-06 13:11:38 +0100

    add details ort

---
+++ src/app/kunde/details-kunde/stammdaten/details-ort.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, type OnInit } from '@angular/core';
+import log from 'loglevel';
+
+/**
+ * Komponente f&uuml;r den CSS-Selektor <code>hs-details-ort</code>
+ */
+@Component({
+    selector: 'hs-details-ort',
+    templateUrl: './details-ort.component.html',
+    standalone: true,
+})
+export class DetailsOrtComponent implements OnInit {
+    @Input()
+    ort: string;
+
+    ngOnInit() {
+        log.debug('DetailsOrtComponent.ort=', this.ort);
+    }
+}
