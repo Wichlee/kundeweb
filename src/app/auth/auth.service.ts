8f55d986a5c27a7a4f320f50fcfb2130221e549c
Erik Dwornik <dwer1011@h-ka.de>
2023-01-07 13:56:26 +0000

    Revert "changed port of app server to 8080"
    This reverts commit 58e174c0007fa6edb32e8f84ae0bd268cb96c9c0.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -23,7 +23,6 @@ import {
 import { first, tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { JwtService } from './jwt.service'; // eslint-disable-line @typescript-eslint/consistent-type-imports
-import { BasicAuthService } from './basic-auth.service';
 import { Subject } from 'rxjs';
 import log from 'loglevel';
 
