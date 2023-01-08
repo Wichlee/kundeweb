9040630f9f2350e65bb6a52b72b266c45afd146c
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2023-01-08 22:57:27 +0100

    add: extend modules

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,27 @@
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule],
-    exports: [MatButtonModule, MatIconModule],
+    imports: [
+        MatButtonModule,
+        MatCardModule,
+        MatDividerModule,
+        MatIconModule,
+        MatProgressBarModule,
+        MatToolbarModule,
+    ],
+    exports: [
+        MatButtonModule,
+        MatCardModule,
+        MatDividerModule,
+        MatIconModule,
+        MatProgressBarModule,
+        MatToolbarModule,
+    ],
 })
 export class MaterialModule {}
