e0c8dbbc844f782acb2efac4f4b74b76c9ab55e2
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2023-01-09 11:44:50 +0100

    add: angular material modules

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,13 @@
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 
@@ -11,16 +16,26 @@ import { NgModule } from '@angular/core';
         MatButtonModule,
         MatCardModule,
         MatDividerModule,
+        MatFormFieldModule,
+        MatGridListModule,
         MatIconModule,
+        MatMenuModule,
         MatProgressBarModule,
+        MatSidenavModule,
+        MatStepperModule,
         MatToolbarModule,
     ],
     exports: [
         MatButtonModule,
         MatCardModule,
         MatDividerModule,
+        MatFormFieldModule,
+        MatGridListModule,
         MatIconModule,
+        MatMenuModule,
         MatProgressBarModule,
+        MatSidenavModule,
+        MatStepperModule,
         MatToolbarModule,
     ],
 })
