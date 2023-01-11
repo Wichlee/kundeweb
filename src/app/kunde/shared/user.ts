efbf80cd02b34613aeb879817900d7e65905177b
Jan <rija1027@h-ka.de>
2023-01-11 14:47:32 +0100

    add user.ts

---
+++ src/app/kunde/shared/user.ts
@@ -0,0 +1,8 @@
+/**
+ * Model als Plain-Old-JavaScript-Object (POJO) fuer die Daten *UND*
+ * Functions fuer Abfragen und Aenderungen.
+ */
+export interface User {
+    username: string;
+    password: string;
+}
