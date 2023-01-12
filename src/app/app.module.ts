2ff8c942c000a03d13207a6c9cde60239abdccaa
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2023-01-12 14:37:37 +0100

    fix: imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { MainComponent } from './layout/main/main.component';
 import { MaterialModule } from './material.module';
 import { NgModule } from '@angular/core';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [MaterialModule],
+    imports: [FooterComponent, HeaderComponent, MainComponent, MaterialModule],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
